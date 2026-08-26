4c4680276cb26f507c067a3ace30ef41d18d3b32
acostarella22 <acostarella22@gmail.com>
2026-08-26 09:31:45 -0400

    js update goal status

--- script.js
+++ script.js
@@ -4,8 +4,10 @@ const goalStatus = document.querySelector("#goal-status");
 goals.forEach(function(goal) {
     goal.addEventListener("click", function() {
         const goalName = goal.querySelector("span").textContent;
+        const checkbox = goal.querySelector("input");
         
         console.log(goalName + " was clicked!");
+        console.log(checkbox.checked);
         
         goalStatus.textContent = goalName + " completed!";
     });
